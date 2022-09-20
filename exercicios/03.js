458a659d2ca912e277928c62df2d609636e9f5d2
Felipe dos Santos <felipesan.cwb@gmail.com>
2022-09-20 10:32:12 -0300

    first commit

---
+++ exercicios/03.js
@@ -0,0 +1,4 @@
+/**
+ * Crie uma função que recebe dois parâmetros, base e expoente,
+ * e retorne a base elevada ao expoente.
+ */
---
+++ exercicios/24.js
@@ -0,0 +1,4 @@
+/**
+ * Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma 
+ * estrutura de repetição while.
+ */
